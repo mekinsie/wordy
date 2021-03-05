dd61bd2f816f0b928ab87d58dd0224d8cdff2851
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-05 13:29:09 -0800

    Create home page.

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ require('pry')
 also_reload('lib/**/*.rb')
 
 get('/') do
+  @word_list = Word.all
+  erb(:home)
+end
+
+get('/new') do
+  erb(:new_word_form)
+end
+
+post('/') do
+  word1 = Word.new(params(:new_word), params(:definition), nil)
+  word1.save
+  @word_list = Word.all
   erb(:home)
 end
