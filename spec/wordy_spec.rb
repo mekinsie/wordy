a76602331ab565ed615e6edfd304f6181b10d38f
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-05 11:19:59 -0800

    Pass third test to set same words to be equal.

--- spec/wordy_spec.rb
+++ spec/wordy_spec.rb
@@ -20,4 +20,12 @@ describe '#Word' do
     end
   end
 
+  describe('#==') do
+    it("is the same word if it has the same attributes as another word") do
+      word1 = Word.new("Hello", "Used as a greeting.", nil)
+      word2 = Word.new("Hello", "Used as a greeting.", nil)
+      expect(word1).to(eq(word2))
+    end
+  end
+
 end
