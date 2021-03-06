1296b964524e8bd6cc653a0dcc3444048e3c62e2
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-05 16:01:26 -0800

    Remove definition attr from word class.

--- lib/wordy.rb
+++ lib/wordy.rb
@@ -1,13 +1,12 @@
 require 'pry'
 
 class Word
-  attr_reader :word, :definition, :id
+  attr_reader :word, :id
   @@words = {}
   @@total_rows = 0
 
-  def initialize(word, definition, id)
+  def initialize(word, id)
     @word = word
-    @definition
     @id = id || @@total_rows +=1
   end
 
@@ -16,7 +15,7 @@ class Word
   end
 
   def save
-    @@words[self.id] = Word.new(self.word, self.definition, self.id)
+    @@words[self.id] = Word.new(self.word, self.id)
   end
 
   def ==(word_compare)
@@ -36,10 +35,7 @@ class Word
     @word = new_word
   end
 
-  def update_def(new_def)
-    @definition = new_def
-  end
-  
+
   def delete
     @@words.delete(self.id)
   end
