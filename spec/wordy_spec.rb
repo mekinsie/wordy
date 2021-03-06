1c6e6eccb54b0383f614c182d628c5bc5396597a
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-05 16:02:51 -0800

    Remove definition attr from specs.

--- spec/wordy_spec.rb
+++ spec/wordy_spec.rb
@@ -16,9 +16,9 @@ end
 
   describe("#save") do
     it("saves a new word to the database") do
-      word1 = Word.new("Hello", "Used as a greeting.", nil)
+      word1 = Word.new("Hello", nil)
       word1.save
-      word2 = Word.new("Goodbye", "Used to express good wishes when parting.", nil)
+      word2 = Word.new("Goodbye", nil)
       word2.save
       expect(Word.all).to(eq([word1, word2]))
     end
@@ -26,17 +26,17 @@ end
 
   describe('#==') do
     it("is the same word if it has the same attributes as another word") do
-      word1 = Word.new("Hello", "Used as a greeting.", nil)
-      word2 = Word.new("Hello", "Used as a greeting.", nil)
+      word1 = Word.new("Hello", nil)
+      word2 = Word.new("Hello", nil)
       expect(word1).to(eq(word2))
     end
   end
 
   describe('.clear') do
     it("clears all words from the database") do
-      word1 = Word.new("Hello", "Used as a greeting.", nil)
+      word1 = Word.new("Hello", nil)
       word1.save
-      word2 = Word.new("Goodbye", "Used to express good wishes when parting.", nil)
+      word2 = Word.new("Goodbye", nil)
       word2.save
       Word.clear
       expect(Word.all).to(eq([]))
@@ -45,7 +45,7 @@ end
 
   describe('.find') do
     it('should find a word based on its id') do
-      word1 = Word.new("Hello", "Used as a greeting.", nil)
+      word1 = Word.new("Hello", nil)
       word1.save
       expect(Word.find(word1.id)).to(eq(word1))
     end
@@ -53,31 +53,32 @@ end
 
   describe('#update_word') do
     it("should update a word") do
-      word1 = Word.new("Hello", "Used as a greeting.", nil)
+      word1 = Word.new("Hello", nil)
       word1.save
       word1.update_word("Goodbye")
       expect(word1.word).to(eq("Goodbye"))
     end
   end
-  describe('#update_def') do
-    it("should update a word's definition") do
-      word1 = Word.new("Hello", "Used as a greeting.", nil)
-      word1.save
-      word1.update_def("Used to express good wishes when parting.")
-      expect(word1.definition).to(eq("Used to express good wishes when parting."))
-    end
-  end
 
   describe("#delete") do
-    it("should delete a word") do
-    word1 = Word.new("Hello", "Used as a greeting.", nil)
+    it("should delete a word by its id") do
+    word1 = Word.new("Hello", nil)
     word1.save
-    word2 = Word.new("Goodbye", "Used to express good wishes when parting.", nil)
+    word2 = Word.new("Goodbye", nil)
     word2.save
     word1.delete
     expect(Word.all).to(eq([word2]))
     end
   end
   
+  # describe(".find_by_word") do
+  #   it("should find a word by its word") do
+  #   word1 = Word.new("Hello", nil)
+  #   word1.save
+  #   word2 = Word.new("Goodbye", nil)
+  #   word2.save
+  #   expect(Word.find_by_word("Hello")).to(eq(1))
+  #   end
+  # end
 
 end
