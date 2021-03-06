c5a144b9edb4f7d651982208fecbbc6473a2773d
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-05 19:05:04 -0800

    Add definition page.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/wordy')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
 
@@ -14,8 +15,11 @@ get('/new') do
 end
 
 post('/') do
-  word1 = Word.new(params[:new_word], params[:definition], nil)
+  binding.pry
+  word1 = Word.new(params[:new_word], nil)
   word1.save
+  def1 = Definition.new(params[:definition], word1.id, nil)
+  def1.save
   @word_list = Word.all
   erb(:home)
 end
@@ -23,4 +27,17 @@ end
 get('/:id') do
   @word = Word.find(params[:id].to_i)
   erb(:word)
+end
+
+get('/add_definition/:id') do
+  @word = Word.find(params[:id].to_i)
+  erb(:new_definition)
+end
+
+post('/definitions') do
+  @word = Word.find(params[:id].to_i)
+  def_new = Definition.new(params[:new_def], @word.id, nil)
+  def_new.save
+  @def_list = Definition.all
+  erb(:definitions)
 end
--- spec/wordy_integration_spec.rb
+++ spec/wordy_integration_spec.rb
@@ -2,15 +2,3 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
-
-
-describe('create a tamagotchi', {:type => :feature}) do
-  it ('creates a tamagotchi') do
-    visit('/')
-    fill_in('name', :with => 'lil tamy')
-    save_and_open_page
-    click_on('Birth my Tamagotchi!')
-    expect(page).to have_content('lil tamy')
-    expect(page).to have_content('Food level')
-  end
-end
