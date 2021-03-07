92d9c93f023da784c1fba0184cf407295736224f
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-07 13:40:20 -0800

    Add integration spec for create a word.

--- spec/wordy_integration_spec.rb
+++ spec/wordy_integration_spec.rb
@@ -2,3 +2,14 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('create a word path', {:type => :feature}) do
+  it('creates a word and then goes to the home page that shows the word') do
+    visit('/')
+    click_on('Add a new word')
+    fill_in('new_word', :with => 'Hello')
+    fill_in('definition', :with => 'A greeting')
+    click_on('Create!')
+    expect(page).to have_content('Hello')
+  end
+end
